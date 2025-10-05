e9faacc3018a53598a57eb54f987cf4bab514e8d
zakbay <zacharybay@live.com>
2025-10-05 03:55:10 -0700

    Refactor Supabase client initialization for improved clarity

--- supabase/supabase.ts
+++ supabase/supabase.ts
@@ -1,6 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
 import { AsyncStorageAdapter } from '@/services/storage.utils'
 import { Database } from '@/supabase/supa-schema'
-import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
